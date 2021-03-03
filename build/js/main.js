62f435acbb697c750ce16a76847543f55eff1cd0
Blazelip <filin1994@rambler.ru>
2021-03-03 18:06:49 +0300

    Правки после ревью (3)

--- build/js/main.js
+++ build/js/main.js
@@ -102,10 +102,24 @@
 (() => {
   const phoneInputs = document.querySelectorAll(`[name="phone"]`);
   const PHONE_MASK = ['+', '7', '(', /[1-9]/, /\d/, /\d/, ')', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/];
+  const PHONE_NUMBER_MIN_LENGTH = 16;
+
+  const validatePhoneNumber = (input) => {
+    if (input.value.length < PHONE_NUMBER_MIN_LENGTH) {
+      input.setCustomValidity(`Введите номер полностью`);
+    } else {
+      input.setCustomValidity(``);
+    }
+  };
 
     if (phoneInputs) {
       phoneInputs.forEach((item) => {
+        item.addEventListener(`keyup`, () => {
+          validatePhoneNumber(item);
+        });
+
         vanillaTextMask.maskInput({
+          guide: false,
           inputElement: item,
           mask: PHONE_MASK,
           showMask: false
